848eb9dbda52942edafb9502e661b084648cdb39
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-05-29 22:21:25 -0500

    buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,56 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Calculator implements ActionListener {
-    public static void main(String[] args) {
 
+    JFrame frame;
+    JTextField textField;
+    JButton[] numbers;
+    JButton[] operators;
+    JButton addition, subtraction, multiplication, division, sqr, sqrRoot, clear, delete, equation;
+    JPanel panel;
+    Font font;
+
+    public Calculator() {
+        /*** window ***/
+        frame = new JFrame("Calculator");
+        frame.setSize(500, 500);
+
+        /*** text ***/
+        textField =  new JTextField();
+        textField.setBounds(50,25,400,50);
+        font = new Font("Times New Roman",Font.BOLD,30);
+        textField.setFont(font);
+        frame.add(textField);
+
+        //textField.setEditable(false);
+
+        /*** BUTTONS ***/
+        //Operator buttons
+        addition = new JButton("+");
+        subtraction = new JButton("-");
+        multiplication = new JButton("*");
+        division = new JButton("/");
+        sqr = new JButton("^2");
+        sqrRoot = new JButton("^(1/2)");
+        clear = new JButton("C");
+        delete = new JButton("<-");
+        equation = new JButton("=");
+
+        //operators[0]=
+
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
+    }
+
+    public static void main(String[] args) {
+    Calculator calculator = new Calculator();
     }
 }
