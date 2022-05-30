d0555f11763ad3e19cdcec194ef72c343c0c64b4
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-05-29 20:35:10 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,13 @@
-public class Calculator {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculator implements ActionListener {
     public static void main(String[] args) {
-        System.out.println("");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
 }
